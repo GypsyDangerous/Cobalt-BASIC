657f4ecd0694d4e5ac9cb9d995a6fc211d73afed
David Snyder <45735850+GypsyDangerous@users.noreply.github.com>
2019-11-08 08:24:21 -0500

    splitting into multiple files

--- basic.py
+++ basic.py
@@ -5,6 +5,8 @@
 from string_with_arrows import *
 import string
 import math
+from Token import *
+
 
 ##############################################################################################
 # CONSTANTS
@@ -59,37 +61,6 @@ TT_EOF = "EOF"
 TT_COMMA = "COMMA"
 TT_ARROW = "ARROW"
 TT_POW = "POW"
-
-
-##############################################################################################
-# TOKEN
-##############################################################################################
-
-
-class Token:
-	def __init__(self, type, val=None, pos_start = None, pos_end = None):
-		self.type = type
-		self.value = val
-		if pos_start:
-			self.pos_start = pos_start.copy()
-			self.pos_end = pos_start.copy()
-			self.pos_end.advance()
-
-		if pos_end:
-			self.pos_end = pos_end
-
-	def matches(self, type_, value):
-		if not type(value) in [list, tuple]:
-			return self.type == type_ and self.value == value
-		else:
-			return self.type == type_ and self.value in value
-
-	def __str__(self):
-		return f"{self.type}: {self.value}" if self.value else f"{self.type}"
-
-	def __repr__(self):
-		return str(self)
-
 
 ##############################################################################################
 # ERRORS
